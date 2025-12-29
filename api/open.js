0dfec7a8d4a25fa6031c09d3b284b6f7b55b14a0
Michael Martin <96404599+mikemartinlfs@users.noreply.github.com>
2025-12-28 22:02:24 -0500

    Update open.js

--- api/open.js
+++ api/open.js
@@ -1,6 +1,9 @@
 import { Redis } from "@upstash/redis";
 
-const redis = Redis.fromEnv();
+const redis = new Redis({
+  url: process.env.UPSTASH_REDIS_REST_URL,
+  token: process.env.UPSTASH_REDIS_REST_TOKEN
+});
 
 export default async function handler(req, res){
   const token=(req.query.t || "").trim();
